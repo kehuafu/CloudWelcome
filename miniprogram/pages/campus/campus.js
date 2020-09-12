4ee0ad61e199d0231b292ac1778b9618574521eb
1593238887@qq.com <ke1166039449>
2020-09-13 00:12:10 +0800

    [feature][校园风景]：加入云数据库以及云存储，优化页面加载

--- miniprogram/pages/campus/campus.js
+++ miniprogram/pages/campus/campus.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    campus: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    const db = wx.cloud.database()
+    db.collection('campus').get({
+      success: function (res) {
+        // res.data 包含该记录的数据
+        console.log(res.data)
+        that.setData({
+          campus: res.data
+        })
+      }
+    })
   },
 
   /**
