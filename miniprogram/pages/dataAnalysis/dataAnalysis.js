2c3939d2a5195730c0449eafb714b382d53c8f1d
1593238887@qq.com <ke1166039449>
2020-09-13 01:23:52 +0800

    [feature][数据分析]：加入云数据库，减少代码冗余

--- miniprogram/pages/dataAnalysis/dataAnalysis.js
+++ miniprogram/pages/dataAnalysis/dataAnalysis.js
@@ -11,60 +11,8 @@ Page({
       '全校各学院人数比例饼状图',
       '全校男女比例饼状图'
     ],
-    option: {
-      "dataset": {
-        "dimensions": ["product", "男", "女"],
-        "source": [{
-            "男": 43,
-            "女": 10,
-            "product": "计院"
-          },
-          {
-            "男": 13,
-            "女": 52,
-            "product": "管院"
-          },
-          {
-            "男": 13,
-            "女": 85,
-            "product": "珠宝"
-          },
-          {
-            "男": 13,
-            "女": 200,
-            "product": "电汽"
-          },
-          {
-            "男": 13,
-            "女": 150,
-            "product": "经院"
-          },
-          {
-            "男": 13,
-            "女": 85,
-            "product": "汽院"
-          },
-          {
-            "男": 13,
-            "女": 50,
-            "product": "土木"
-          },
-        ]
-      },
-      "legend": {},
-      "series": [{
-        "type": "bar",
-        "color": "#2E7DC8"
-      }, {
-        "type": "bar",
-        "color": "#F65B57"
-      }],
-      "tooltip": {},
-      "xAxis": {
-        "type": "category"
-      },
-      "yAxis": {}
-    }
+    option: '',
+    data: ''
   },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -72,187 +20,25 @@ Page({
       case "0":
         this.setData({
           index: 0,
-          option: {
-            "dataset": {
-              "dimensions": ["product", "男", "女"],
-              "source": [{
-                  "男": 43,
-                  "女": 10,
-                  "product": "计院"
-                },
-                {
-                  "男": 13,
-                  "女": 52,
-                  "product": "管院"
-                },
-                {
-                  "男": 13,
-                  "女": 85,
-                  "product": "珠宝"
-                },
-                {
-                  "男": 13,
-                  "女": 200,
-                  "product": "电汽"
-                },
-                {
-                  "男": 13,
-                  "女": 150,
-                  "product": "经院"
-                },
-                {
-                  "男": 13,
-                  "女": 85,
-                  "product": "汽院"
-                },
-                {
-                  "男": 13,
-                  "女": 50,
-                  "product": "土木"
-                },
-              ]
-            },
-            "legend": {},
-            "series": [{
-              "type": "bar",
-              "color": "#2E7DC8"
-            }, {
-              "type": "bar",
-              "color": "#F65B57"
-            }],
-            "tooltip": {},
-            "xAxis": {
-              "type": "category"
-            },
-            "yAxis": {}
-          }
+          option: this.data.data[0]
         })
         break;
       case "1":
         this.setData({
           index: 1,
-          option: {
-            "color": ["#209BAA"],
-            "grid": {
-              "bottom": "3%",
-              "containLabel": true,
-              "left": "3%",
-              "right": "4%"
-            },
-            "series": [{
-              "barWidth": "60%",
-              "data": [10, 52, 200, 334, 390, 330, 220],
-              "name": "人数",
-              "type": "bar"
-            }],
-            "tooltip": {
-              "axisPointer": {
-                "type": "shadow"
-              },
-              "trigger": "axis"
-            },
-            "xAxis": [{
-              "axisTick": {
-                "alignWithLabel": true
-              },
-              "data": ["计院", "管院", "珠宝", "电汽", "经院", "汽院", "土木"],
-              "type": "category"
-            }],
-            "yAxis": [{
-              "type": "value"
-            }]
-          }
+          option: this.data.data[1]
         })
         break;
       case "2":
         this.setData({
           index: 2,
-          option: {
-            "legend": {
-              "data": ["计院", "管院", "珠宝", "电汽", "经院"],
-              "left": "left",
-              "orient": "vertical"
-            },
-            "series": [{
-              "center": ["50%", "60%"],
-              "data": [{
-                "name": "计院",
-                "value": 335
-              }, {
-                "name": "管院",
-                "value": 310
-              }, {
-                "name": "珠宝",
-                "value": 234
-              }, {
-                "name": "电汽",
-                "value": 135
-              }, {
-                "name": "经院",
-                "value": 1548
-              }],
-              "emphasis": {
-                "itemStyle": {
-                  "shadowBlur": 10,
-                  "shadowColor": "rgba(0, 0, 0, 0.5)",
-                  "shadowOffsetX": 0
-                }
-              },
-              "name": "访问来源",
-              "radius": "55%",
-              "type": "pie"
-            }],
-            "title": {
-              "left": "center",
-              "subtext": "模拟数据",
-              "text": ""
-            },
-            "tooltip": {
-              "formatter": "{b} : {c} ({d}%)",
-              "trigger": "item"
-            }
-          }
+          option: this.data.data[2]
         })
         break;
       case "3":
         this.setData({
           index: 3,
-          option: {
-            "legend": {
-              "data": ["男", "女"],
-              "left": "left",
-              "orient": "vertical"
-            },
-            "series": [{
-              "center": ["50%", "60%"],
-              "data": [{
-                "name": "男",
-                "value": 13350
-              }, {
-                "name": "女",
-                "value": 10100
-              }],
-              "emphasis": {
-                "itemStyle": {
-                  "shadowBlur": 10,
-                  "shadowColor": "rgba(0, 0, 0, 0.5)",
-                  "shadowOffsetX": 0
-                }
-              },
-              "name": "访问来源",
-              "radius": "55%",
-              "type": "pie"
-            }],
-            "title": {
-              "left": "center",
-              "subtext": "模拟数据",
-              "text": ""
-            },
-            "tooltip": {
-              "formatter": "{b} : {c} ({d}%)",
-              "trigger": "item"
-            }
-          }
+          option: this.data.data[3]
         })
         break;
     }
@@ -262,15 +48,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    const db = wx.cloud.database()
+    db.collection('dataAnalysis').get({
+      success: function (res) {
+        // res.data 包含该记录的数据
+        console.log(res.data)
+        that.setData({
+          data: res.data,
+          option: res.data[0]
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
